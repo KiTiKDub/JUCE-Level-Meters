3ae0d73a8061872abd869bc49ad5958284630df3
Kyle Whaley <kitikdub@gmail.com>
2023-11-03 18:12:20 -0700

    Fixed Decleration and Implementation clash

--- Source/LevelMeter.h
+++ Source/LevelMeter.h
@@ -13,7 +13,30 @@
 
 struct LevelMeter : juce::Component
 {
-    void paint(juce::Graphics& g) override;
+    void paint(juce::Graphics& g) override
+    {
+        using namespace juce;
+
+            //shapes the meters. May be a bit inefficeint, not sure the best way to move this stuff around, but it is there.
+            auto bounds = getLocalBounds().toFloat();
+            bounds = bounds.removeFromLeft(bounds.getWidth() * .8);
+            bounds = bounds.removeFromRight(bounds.getWidth() * .75);
+            bounds = bounds.removeFromTop(bounds.getHeight() * .9);
+            bounds = bounds.removeFromBottom(bounds.getHeight() * .88);
+
+            //get our base rectangle
+            g.setColour(Colours::grey);
+            g.fillRect(bounds);
+
+            //get gradient
+            auto gradient = ColourGradient::ColourGradient(Colours::green, bounds.getBottomLeft(), Colours::red, bounds.getTopLeft(), false);
+            gradient.addColour(.5f, Colours::yellow);
+            g.setGradientFill(gradient);
+
+            //Show gradient
+            auto levelMeterFill = jmap(level, -60.f, +6.f, 0.f, static_cast<float>(bounds.getHeight()));
+            g.fillRoundedRectangle(bounds.removeFromBottom(levelMeterFill), 5.f);
+    };
 
     //default value so the meters  are black when the plugin is launched
     void setLevel(float value) { level = value; }
@@ -21,31 +44,6 @@ struct LevelMeter : juce::Component
 private:
     float level = -60.f;
 };
-
-void LevelMeter::paint(juce::Graphics& g)
-{
-    using namespace juce;
-
-    //shapes the meters. May be a bit inefficeint, not sure the best way to move this stuff around, but it is there.
-    auto bounds = getLocalBounds().toFloat();
-    bounds = bounds.removeFromLeft(bounds.getWidth() * .8);
-    bounds = bounds.removeFromRight(bounds.getWidth() * .75);
-    bounds = bounds.removeFromTop(bounds.getHeight() * .9);
-    bounds = bounds.removeFromBottom(bounds.getHeight() * .88);
-
-    //get our base rectangle
-    g.setColour(Colours::grey);
-    g.fillRect(bounds);
-
-    //get gradient
-    auto gradient = ColourGradient::ColourGradient(Colours::green, bounds.getBottomLeft(), Colours::red, bounds.getTopLeft(), false);
-    gradient.addColour(.5f, Colours::yellow);
-    g.setGradientFill(gradient);
-
-    //Show gradient
-    auto levelMeterFill = jmap(level, -60.f, +6.f, 0.f, static_cast<float>(bounds.getHeight()));
-    g.fillRoundedRectangle(bounds.removeFromBottom(levelMeterFill), 5.f);
-}
 
 /*
 =============================================================================================================
--- Source/LevelMeterData.h
+++ Source/LevelMeterData.h
@@ -3,34 +3,23 @@
 
 struct LevelMeterData
 {
-	void process(bool inOrOut, int channel, juce::AudioBuffer<float>& buffer);
-	float getRMS(int channel);
-	float getOutRMS(int channel);
+	void process(bool inOrOut, int channel, juce::AudioBuffer<float>& buffer)
+	{
+		if (inOrOut) //true is in rms, false is out rms
+		{
+			rmsIn[channel] = juce::Decibels::gainToDecibels(buffer.getRMSLevel(channel, 0, buffer.getNumSamples()));
+		}
+		else
+		{
+			rmsOut[channel] = juce::Decibels::gainToDecibels(buffer.getRMSLevel(channel, 0, buffer.getNumSamples()));
+		}
+	};
+
+	float getRMS(int channel) {return rmsIn[channel]};
+	float getOutRMS(int channel) {return rmsOut[channel]};
 
 private:
 
 	std::array<std::atomic<float>, 2> rmsIn;
 	std::array<std::atomic<float>, 2> rmsOut;
 };
-
-void LevelMeterData::process(bool inOrOut, int channel, juce::AudioBuffer<float>& buffer)
-{
-	if (inOrOut) //true is in rms, false is out rms
-	{
-		rmsIn[channel] = juce::Decibels::gainToDecibels(buffer.getRMSLevel(channel, 0, buffer.getNumSamples()));
-	}
-	else
-	{
-		rmsOut[channel] = juce::Decibels::gainToDecibels(buffer.getRMSLevel(channel, 0, buffer.getNumSamples()));
-	}
-}
-
-float LevelMeterData::getRMS(int channel)
-{
-	return rmsIn[channel];
-}
-
-float LevelMeterData::getOutRMS(int channel)
-{
-	return rmsOut[channel];
-}
